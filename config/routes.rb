e33db6d917baed4d9d5de7a3ab2046f656ece18a
Mathys Meuris <mathys.meuris@gmail.com>
2024-06-05 13:49:14 +0200

    finish

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,17 @@ Rails.application.routes.draw do
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
+  get "index", to: "articles#index"
+
+  get "articles/new", to: "articles#new"
+  post "articles", to: "articles#create"
+
+  get "articles/:id/edit", to: "articles#edit", as: :edit
+  patch "articles/:id", to: "articles#update"
+
+  delete "articles/:id", to: "articles#destroy"
+
+  get "articles/:id", to: "articles#show", as: :article
 
   # Defines the root path route ("/")
   # root "posts#index"
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,18 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+require 'faker'
+
+Article.destroy_all
+
+puts 'enter in faker'
+
+10.times do
+  article = Article.new(
+    title: Faker::Movie.title,
+    content: Faker::Lorem.sentence
+  )
+  article.save!
+end
+
+puts 'faker finish'
